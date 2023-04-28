071e19f2a86713671dec42aa839b27b3e530c8ac
CaduRomaniello <cadu_romaniello@hotmail.com>
2023-04-28 04:39:21 -0300

    feat: changing status bar color

--- challenge-2/App.js
+++ challenge-2/App.js
@@ -12,6 +12,7 @@ export default function App() {
       <SafeAreaView style={styles.container}>
         <SignupScreen/>
       </SafeAreaView>
+      <StatusBar style="light"></StatusBar>
     </View>
   );
 }
