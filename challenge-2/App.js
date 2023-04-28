c29f58760a3764a6b01d1e287a5f1907b5d2dfa6
CaduRomaniello <cadu_romaniello@hotmail.com>
2023-04-28 04:32:45 -0300

    feat: adding checkbox to signupscreen

--- challenge-2/App.js
+++ challenge-2/App.js
@@ -10,7 +10,7 @@ export default function App() {
   return (
     <View style={styles.rootContainer}>
       <SafeAreaView style={styles.container}>
-        <HomeScreen/>
+        <SignupScreen/>
       </SafeAreaView>
     </View>
   );
--- challenge-2/screens/SignupScreen.js
+++ challenge-2/screens/SignupScreen.js
@@ -1,22 +1,30 @@
 // Sign up screen
 
+import Checkbox from 'expo-checkbox';
 import { View, Text, StyleSheet, Pressable } from 'react-native'
 import Input from '../components/Input';
 import {Colors} from '../constants/styles'
 import Button from '../components/Button';
+import React, { useState } from 'react';
 
 function SignupScreen(){
+    const [isChecked, setChecked] = useState(false)
+
     function onPress(){
         console.log("Text pressed")
     }
 
     return <View style={styles.container}>
         <View style={styles.contentContainer}>
-            <View style={styles.inputsView}>
                 <Text style={styles.title}> SIGN UP </Text>
+            <View style={styles.inputsView}>
                 <Input iconPath={require('../assets/icon_mail.png')}  placeHolder='Your email' password={false} keyboardType='email-address'/>
                 <Input iconPath={require('../assets/icon_user.png')}  placeHolder='username' password={false} keyboardType='default'/>
                 <Input iconPath={require('../assets/icon_lock.png')}  placeHolder='Your password' password={true} keyboardType='default'/>
+                <View style={styles.checkboxContainer}>
+                    <Checkbox style={styles.checkbox} value={isChecked} onValueChange={setChecked} color={isChecked ? '#4630EB' : undefined}/>
+                    <Text style={styles.checkboxText}>Agree To <Text style={styles.underlinedText}>Terms And Conditions</Text></Text>
+                </View>
                 <Button btnText='CREATE ACCOUNT'/>
             </View>
         </View>
@@ -46,14 +54,14 @@ const styles = StyleSheet.create({
         textAlign:'center',
         alignItems: 'center',
         // marginTop: 100,
-        marginBottom:100
+        // marginBottom:50
     },
     inputsView:{
         flex: 2,
         width: "100%",
-        alignItems: 'center',
+        alignItems: 'flex-start',
         justifyContent: 'center',
-        textAlign:'center'
+        textAlign:'center',
     },
     footer:{
         paddingTop:20,
@@ -70,5 +78,25 @@ const styles = StyleSheet.create({
         flexDirection:'column',
         alignItems:'center',
         justifyContent: 'center',
+    },
+    checkboxContainer:{
+        width: "100%",
+        flexDirection:'row',
+        marginBottom:32,
+        alignItems:'center',
+        justifyContent: 'flex-start',
+    },
+    checkbox:{
+        marginRight: 10,
+        borderRadius: 5,
+        borderColor:"white",
+        backgroundColor: "white"
+    },
+    checkboxText:{
+        color :'white'
+    },
+    underlinedText:{
+        textDecorationLine: 'underline',
+        fontWeight: 'bold'
     }
 });
